48cf5cccc815689a9e553e702a39e1d5646e3430
songhm7 <1991203@hansung.ac.kr>
2024-04-06 20:28:00 +0900

    Google map api에서 자체 제공하는 현위치 버튼으로 변경, 중복된 일부 코드 정리

--- app/src/main/java/com/hansung/roadbuddyandroid/MainActivity.kt
+++ app/src/main/java/com/hansung/roadbuddyandroid/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.hansung.roadbuddyandroid
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -31,20 +40,42 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var curLocation: LatLng
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
+    private val initialZoomLevel = 17f // 초기 축척 설정
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         // 현위치,GPS 관련 초기화
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        //위치권한 동의
         checkLocationPermission()
+
+        //자동 현위치 업데이트
         setupLocationUpdates()
 
         // 지도 fragment 초기화
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        //축척조절바 초기화
+        mapFragment.getMapAsync { googleMap ->
+            // 사용자 위치 활성화 (내 위치 버튼 포함)
+            googleMap.isMyLocationEnabled = true
+        }
+
+        // 내 위치 버튼의 참조를 얻기
+        val locationButton = (mapFragment.view?.findViewById<View>(Integer.parseInt("1"))?.parent as View).findViewById<View>(Integer.parseInt("2"))
+
+        // 버튼의 위치 속성 조정
+        val layoutParams = locationButton.layoutParams as RelativeLayout.LayoutParams
+
+        // 오른쪽 하단에 위치시키기
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0)
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+        layoutParams.setMargins(0, 0, 30, 30)
+
+        //개발중 사용할 축척조절바 초기화
         scaleSeekBar = findViewById(R.id.scaleSeekBar)
         scaleSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -61,30 +92,29 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 // 사용자가 SeekBar 조작을 멈출 때 호출할 함수
             }
         })
+        val searchingBar = findViewById<TextView>(R.id.searchingBar)
 
-        findViewById<Button>(R.id.buttonToHome).setOnClickListener {
-            checkLocationPermission()
-            mMap.clear()
-            mMap.addMarker(MarkerOptions().position(curLocation).title("현위치"))
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curLocation, 17f))
+        //검색창에 커서올리면 액티비티 이동
+        searchingBar.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
         }
-    }
+
+        }
+
 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // 초기 축척 설정
-        val initialZoomLevel = 17f
-
         // 마커 추가
         mMap.addMarker(MarkerOptions().position(hansung).title("한성대학교"))
-
         // 지도 이동
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hansung, initialZoomLevel))
 
     }
 
+    //위치정보권한확인
     private fun checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED
@@ -115,6 +145,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    //위치정보권한동의
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
@@ -131,20 +162,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                             Manifest.permission.ACCESS_FINE_LOCATION
                         ) == PackageManager.PERMISSION_GRANTED
                     ) {
-                        fusedLocationClient?.lastLocation
-                            ?.addOnSuccessListener(this) { location ->
-                                // GPS가 꺼져있거나 최근 위치 정보가 없을 때 location은 null일 수 있음
-                                if (location != null) {
-                                    curLocation = LatLng(location.latitude, location.longitude)
-                                    // 위치 정보 사용
-                                    Log.d(
-                                        "onRequest 현위치가져오기성공",
-                                        "${location.latitude}, ${location.longitude}"
-                                    )
-                                } else {
-                                    Log.d("onRequestPermissionsResult", "location null값 발생")
-                                }
-                            }
+                        checkLocationPermission()
                     }
                 } else {
                     // 권한이 거부됨. 권한 요청에 대한 처리 필요.
@@ -170,14 +188,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             override fun onLocationResult(locationResult: LocationResult) {
                 locationResult ?: return
                 for (location in locationResult.locations){
-                    // 여기서 위치 정보를 사용하세요.
+                    // 여기서 위치 정보를 사용
                     curLocation = LatLng(location.latitude, location.longitude)
-                    // 예를 들어, 맵을 현재 위치로 이동
-                    //mMap.moveCamera(CameraUpdateFactory.newLatLng(curLocation))
+
                 }
             }
         }
-
         // 위치 업데이트 요청 시작
         startLocationUpdates()
     }
@@ -191,7 +207,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    // 위치 업데이트 중지하는 함수도 추가 필요
+    // 위치 업데이트 중지하는 메소드
     private fun stopLocationUpdates() {
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
